afdda1e9524d784846ebb99ac4b7823da29fc5cd
Suraj Maharjan <surajmzn75@gmail.com>
2019-10-20 13:30:44 +0545

    variable rename and added vailadation

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,27 +3,37 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
+
+    //creating objects
 
     Button btnmultiple,btnsevennum,btneightnum,btnninenum,btnadd,btnfournum,btnfivenum;
     Button btnsixnum,btnsubtract,btnonenum,btntwonum,btnthreenum,btnclear,btnzeronum;
     Button btnequal,btndivide;
     EditText numbertext;
+
     String displayNum ="";
-    String op;
-    int num1 ,num2, res;
-    int abc,abcc;
+
+    int valueOne , valueTwo, res;
+    String symbol;
+    int display;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //fetching values and assigning it our objects
 
         btnonenum  = findViewById(R.id.btnOne);
         btntwonum  = findViewById(R.id.btnTwo);
@@ -47,14 +57,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         btnonenum.setOnClickListener(this);
         btntwonum.setOnClickListener(this);
+        btnthreenum.setOnClickListener(this);
+        btnfournum.setOnClickListener(this);
+        btnfivenum.setOnClickListener(this);
+        btnsixnum.setOnClickListener(this);
+        btnsevennum.setOnClickListener(this);
+        btneightnum.setOnClickListener(this);
+        btnninenum.setOnClickListener(this);
+        btnzeronum.setOnClickListener(this);
+
         btnequal.setOnClickListener(this);
         btnadd.setOnClickListener(this);
-
-
+        btnclear.setOnClickListener(this);
+        btnsubtract.setOnClickListener(this);
+        btndivide.setOnClickListener(this);
+        btnmultiple.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
+
+        if(view.getId()==R.id.btnAdd)
+        {
+            if(TextUtils.isEmpty(displayNum)) {
+                Toast.makeText(this, "Please enter number", Toast.LENGTH_SHORT).show();
+
+
+            }else
+            {
+                valueOne = Integer.parseInt(displayNum);
+                displayNum="";
+                symbol="+";
+            }
+
+
+
+        }
         switch (view.getId())
         {
             case R.id.btnOne:
@@ -67,35 +105,125 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 numbertext.setText(displayNum);
                 break;
 
-            case R.id.btnAdd:
-                num1 = Integer.parseInt(displayNum);
-                displayNum="";
-                op="+";
+            case R.id.btnThree:
+                displayNum += 3;
+                numbertext.setText(displayNum);
                 break;
+
+            case R.id.btnFour:
+                displayNum += 4;
+                numbertext.setText(displayNum);
+                break;
+
+            case R.id.btnFive:
+                displayNum += 5;
+                numbertext.setText(displayNum);
+                break;
+
+            case R.id.btnSix:
+                displayNum += 6;
+                numbertext.setText(displayNum);
+                break;
+
+            case R.id.btnSeven:
+                displayNum += 7;
+                numbertext.setText(displayNum);
+                break;
+
+
+            case R.id.btnEight:
+                displayNum += 8;
+                numbertext.setText(displayNum);
+                break;
+
+
+            case R.id.btnNine:
+                displayNum += 9;
+                numbertext.setText(displayNum);
+                break;
+
+            case R.id.btnZero:
+                displayNum += 0;
+                numbertext.setText(displayNum);
+                break;
+
+
+            case R.id.btnSubtract:
+                if(TextUtils.isEmpty(displayNum)) {
+                    Toast.makeText(this, "Please enter number", Toast.LENGTH_SHORT).show();
+
+                }
+                else {
+                    valueOne = Integer.parseInt(displayNum);
+                    displayNum = "";
+                    symbol = "-";
+                }
+                break;
+
+            case R.id.btnDivide:
+                if(TextUtils.isEmpty(displayNum)) {
+                    Toast.makeText(this, "Please enter number", Toast.LENGTH_SHORT).show();
+
+                }
+                else {
+                    valueOne = Integer.parseInt(displayNum);
+                    displayNum = "";
+                    symbol = "/";
+                }
+                break;
+
+
+            case R.id.btnMultiply:
+                if(TextUtils.isEmpty(displayNum)) {
+                    Toast.makeText(this, "Please enter number", Toast.LENGTH_SHORT).show();
+
+                }
+                else {
+                    valueOne = Integer.parseInt(displayNum);
+                    displayNum = "";
+                    symbol = "*";
+                }
+                break;
+
 
             case R.id.btnEqual:
-                num2 = Integer.parseInt(displayNum);
-                res =getResult(op,num1,num2);
+                if(TextUtils.isEmpty(displayNum)) {
+                    Toast.makeText(this, "Please enter number", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                valueTwo = Integer.parseInt(displayNum);
+                res =getResult(symbol,valueOne,valueTwo);
                 numbertext.setText(String.valueOf(res));
+                }
                 break;
 
+
+            case R.id.btnClear:
+                displayNum = "";
+                numbertext.setText(displayNum);
+                break;
 
         }
     }
 
 
-    private int getResult(String op,int n1, int n2)
+    private int getResult(String symbol,int n1, int n2)
     {
-        switch (op)
+        if(symbol=="+")
         {
-            case "+":
-                return n1+n2;
-
-            case "-":
-                return n1-n2;
-
-                default:
-                    return  0;
+            display= n1+n2;
         }
+        else if(symbol=="-"){
+            display= n1-n2;
+
+        }else if(symbol=="*"){
+            display= n1*n2;
+
+        }else if(symbol=="/"){
+            display= n1/n2;
+
+        }
+
+        return display;
     }
 }
